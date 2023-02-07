24a3ebbb2335e1931e010eab5ba435d5ffd66cd3
Sebastian Nadiałkowski <Sirskuty@gmail.com>
2023-02-07 21:06:24 +0100

    js messing

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ formElement.addEventListener("submit", (event) => {
     const weight = +weightElement.value;
     result = weight/(height*height);
     resultElement.innerText = `${result.toFixed(2)}`;
-    
 
+   
+    if (result<= 16) {
+        document.getElementById("result").innerHTML="wygłodzenie";
+    } else if (result >= 16 && result <= 16,99){
+        document.getElementById("result").innerHTML="wychudzenie";
+    } else if (result >= 17 && result <= 18,49) {
+        document.getElementById("result").innerHTML="niedowaga";
+    } else if (result >= 18,5 && result <= 24,9) {
+        document.getElementById("result").innerHTML="prawidłowa";
+    } else if (result >= 25 && result <= 29,9) {
+    document.getElementById("result").innerHTML="nadwaga";
+    }
 })
