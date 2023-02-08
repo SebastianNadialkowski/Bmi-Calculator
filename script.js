dc2c30c5a41f0dcf6c45eb4b4c2f1efb729f3522
Sebastian Nadiałkowski <Sirskuty@gmail.com>
2023-02-08 18:35:51 +0100

    update

--- script.js
+++ script.js
@@ -8,19 +8,25 @@ formElement.addEventListener("submit", (event) => {
     event.preventDefault();
     const height = +heightElement.value;
     const weight = +weightElement.value;
-    result = weight/(height*height);
+    result = weight / (height * height / 10000);
     resultElement.innerText = `${result.toFixed(2)}`;
 
-   
-    if (result<= 16) {
-        document.getElementById("result").innerHTML="wygłodzenie";
-    } else if (result >= 16 && result <= 16,99){
-        document.getElementById("result").innerHTML="wychudzenie";
-    } else if (result >= 17 && result <= 18,49) {
-        document.getElementById("result").innerHTML="niedowaga";
-    } else if (result >= 18,5 && result <= 24,9) {
-        document.getElementById("result").innerHTML="prawidłowa";
-    } else if (result >= 25 && result <= 29,9) {
-    document.getElementById("result").innerHTML="nadwaga";
+
+    if (result <= 16) {
+        document.getElementById("result").innerHTML = "wygłodzenie";
+    } else if (result >= 16 && result <= 16.99) {
+        document.getElementById("result").innerHTML = "wychudzenie";
+    } else if (result >= 17 && result <= 18.49) {
+        document.getElementById("result").innerHTML = "niedowaga";
+    } else if (result >= 18.5 && result <= 24.9) {
+        document.getElementById("result").innerHTML = "prawidłowe";
+    } else if (result >= 25 && result <= 29.9) {
+        document.getElementById("result").innerHTML = "nadwaga";
+    } else if (result >= 30 && result <= 34.9) {
+        document.getElementById("result").innerHTML = "I stopień otyłości";
+    } else if (result >= 35 && result <= 39.9) {
+        document.getElementById("result").innerHTML = "II stopień otyłości";
+    } else if (result >= 40 && result <= 999999) {
+        document.getElementById("result").innerHTML = "otyłość skrajna";
     }
-})
+    }) 
