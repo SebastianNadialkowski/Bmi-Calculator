2c5ba341acb53281cb786d94632723827f71f49b
Sebastian Nadiałkowski <Sirskuty@gmail.com>
2023-02-10 17:42:11 +0100

    js update

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-const heightElement = document.querySelector(".js-height")
-const weightElement = document.querySelector(".js-weight")
-const resultElement = document.querySelector(".js-result")
-const formElement = document.querySelector(".js-form")
+    {
+        const onFormSubmit = (event) => {
+            event.preventDefault();
+            const heightElement = document.querySelector(".js-height");
+            const weightElement = document.querySelector(".js-weight");
+            const resultElement = document.querySelector(".js-result");
+            const height = +heightElement.value;
+            const weight = +weightElement.value;
+            result = weight / (height * height / 10000);
+            resultElement.innerText = `${result.toFixed(2)}`;
+            if (result <= 16) {
+                document.getElementById("result").innerHTML = "wygłodzenie";
+            } else if (result >= 16 && result <= 16.99) {
+                document.getElementById("result").innerHTML = "wychudzenie";
+            } else if (result >= 17 && result <= 18.49) {
+                document.getElementById("result").innerHTML = "niedowaga";
+            } else if (result >= 18.5 && result <= 24.9) {
+                document.getElementById("result").innerHTML = "prawidłowe";
+            } else if (result >= 25 && result <= 29.9) {
+                document.getElementById("result").innerHTML = "nadwaga";
+            } else if (result >= 30 && result <= 34.9) {
+                document.getElementById("result").innerHTML = "I stopień otyłości";
+            } else if (result >= 35 && result <= 39.9) {
+                document.getElementById("result").innerHTML = "II stopień otyłości";
+            } else if (result >= 40 && result <= 999999) {
+                document.getElementById("result").innerHTML = "otyłość skrajna";
+            }
+        }
 
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const height = +heightElement.value;
-    const weight = +weightElement.value;
-    result = weight / (height * height / 10000);
-    resultElement.innerText = `${result.toFixed(2)}`;
-
-
-    if (result <= 16) {
-        document.getElementById("result").innerHTML = "wygłodzenie";
-    } else if (result >= 16 && result <= 16.99) {
-        document.getElementById("result").innerHTML = "wychudzenie";
-    } else if (result >= 17 && result <= 18.49) {
-        document.getElementById("result").innerHTML = "niedowaga";
-    } else if (result >= 18.5 && result <= 24.9) {
-        document.getElementById("result").innerHTML = "prawidłowe";
-    } else if (result >= 25 && result <= 29.9) {
-        document.getElementById("result").innerHTML = "nadwaga";
-    } else if (result >= 30 && result <= 34.9) {
-        document.getElementById("result").innerHTML = "I stopień otyłości";
-    } else if (result >= 35 && result <= 39.9) {
-        document.getElementById("result").innerHTML = "II stopień otyłości";
-    } else if (result >= 40 && result <= 999999) {
-        document.getElementById("result").innerHTML = "otyłość skrajna";
+        const init = () => {
+            const formElement = document.querySelector(".js-form");
+            formElement.addEventListener("submit", onFormSubmit); 
+        }
+        init();
     }
-    }) 
+
+
+
+
+  
